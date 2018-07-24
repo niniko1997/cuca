ddc61255b26c4157503ad3de705b8c3fa2e2d59c
niniko1997 <m.nino97@gmail.com>
2018-07-24 17:33:49 -0400

    min func responsive navbar

--- script/script.js
+++ script/script.js
@@ -5,3 +5,24 @@ $(document).ready(function() {
     }, 3000);
  
 });
+
+function toggle_navbar() {
+    var x = document.getElementById("header_wrapper");
+    if (x.className === "header_wrapper") {
+        x.className += " responsive";
+    } else {
+        x.className = "header_wrapper";
+    }
+    var x = document.getElementById("mobile-navigation");
+    if (x.className === "") {
+        x.className += " responsive";
+    } else {
+        x.className = "";
+    }
+    var x = document.getElementById("header");
+    if (x.className === "") {
+        x.className += " responsive";
+    } else {
+        x.className = "";
+    }
+}
