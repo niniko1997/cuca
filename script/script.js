b8703ac9dc2810addfe1ebff0be80bd45c682f1a
niniko1997 <m.nino97@gmail.com>
2018-07-25 14:00:38 -0400

    changed background mobile navbar

--- script/script.js
+++ script/script.js
@@ -13,12 +13,14 @@ function toggle_navbar() {
     } else {
         x.className = "header_wrapper";
     }
+
     var x = document.getElementById("mobile-navigation");
     if (x.className === "") {
         x.className += " responsive";
     } else {
         x.className = "";
     }
+
     var x = document.getElementById("header");
     if (x.className === "") {
         x.className += " responsive";
